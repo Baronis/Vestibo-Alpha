82e652ea0a8fb6e0e53fb8888fbcadf45222eceb
Matheus Baroni <matheuspbaroni@gmail.com>
2015-01-20 17:08:44 -0200

    Pequenas alterações

--- in/js/popup.js
+++ in/js/popup.js
@@ -3,8 +3,8 @@
         Última revisão: 15/01/2014 (Matheus Baroni)
 */
 function close_popup(){
-   var a = document.getElementById('popup');
-   a.style.display = 'none';
+   var popup = document.getElementById('popup');
+   popup.style.display = 'none';
 }
 /*
         ===============================================================================
